a2d86329fcc47bf75e850515436b820f26d2048d
Felipe Lima <flf.felipe@gmail.com>
2019-05-16 23:03:49 -0300

    Adds connection to Mongodb

--- aula1/app.js
+++ aula1/app.js
@@ -1,5 +1,27 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const url = 'mongodb://127.0.0.1:27017/mydb'
+const options = { reconnectTries: Number.MAX_VALUE, reconnectInterval: 500, poolSize: 5, useNewUrlParser: true };
+
+mongoose.connect(url, options);
+mongoose.set('useCreateIndex', true);
+
+mongoose.connection.on('error', (err) => {
+    console.log('Erro na conexão com o banco de dados: ' + err);
+});
+
+mongoose.connection.on('disconnected', (err) => {
+    console.log('Aplicação desconectada do banco de dados');
+});
+mongoose.connection.on('connected', (err) => {
+    console.log('Aplicação conectada do banco de dados');
+});
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json);
 
 const indexRoute = require('./routes/index');
 const usersRoute = require('./routes/users');
