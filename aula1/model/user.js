7e56f87050006128dddba83a63bd07baa4500128
Felipe Lima <flf.felipe@gmail.com>
2019-05-19 19:16:24 -0300

    Encrypts password

--- aula1/model/user.js
+++ aula1/model/user.js
@@ -1,10 +1,25 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;  
+const bcrypt = require('bcryptjs');
 
 const UserSchema = new Schema({
     email: {type: String, required: true, unique: true, lowercase: true},
     password: {type: String, required: true, select: false},
     created: {type: Date, default: Date.now}
+});
+
+UserSchema.pre('save', function(next) {
+    let user = this;
+    console.log(`USER: ${user}`)
+
+    if (!user.isModified('password')) return next();
+
+    console.log("encriptando...");
+
+    bcrypt.hash(user.password, 10, (err, encrypted) => {
+        user.password = encrypted;
+        return next();
+    });
 });
 
 module.exports = mongoose.model('User', UserSchema);
--- aula1/routes/users.js
+++ aula1/routes/users.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router();
-const Users = require('../model/user')
+const Users = require('../model/user');
+const bcrypt = require('bcryptjs');
 
 router.get('/', (req, res) => {
     console.log("entrou no search")
     Users.find({}, (err, data) => {
         console.log("procurando...")
-        if (err) return res.send({ error: "Erro na consulta de usuários!" });
+        if (err) return res.status(500).send({ error: "Erro na consulta de usuários!" });
         return res.send(data);
     });
 });
@@ -19,14 +20,30 @@ router.post('/create', (req, res) => {
     if (!email || !password) return res.send({ error: "Dados insuficientes!" });
 
     Users.findOne({ email }, (err, data) => {
-        if (err) res.send({ error: "Erro ao buscar usuário!" });
+        if (err) return res.status(500).send({ error: "Erro ao buscar usuário!" });
         if (data) return res.send({ error: "Usuário já cadastrado!" });
 
         Users.create(req.body, (err, data) => {
-            if (err) return res.send({ error: "Erro ao criar usuário!"});
+            if (err) return res.status(500).send({ error: "Erro ao criar usuário!"});
             return res.send(data);
         });
     });
+});
+
+router.post("/auth", (req, res) => {
+    console.log(`Entrou no auth ${JSON.stringify(req.body)}`);
+    const { email, password } = req.body;
+    console.log(`email: ${email}, password: ${password}`)
+
+    Users.findOne({ email }, (err, data) => {
+        if (err) return res.status(500).send({ error: "Erro ao buscar usuário!" });
+        if (!data) return res.send({ error: "Usuário não cadastrado!" });
+
+        bcrypt.compare(password, data.password, (err, same) => {
+            if (!same) return res.status(500).send({ error: "Erro ao autenticar usuário!" });
+            return res.send(data);
+        });
+    }).select('+password');
 });
 
 module.exports = router;
